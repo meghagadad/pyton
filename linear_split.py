1117abc8bc5b64ec0ffd0ab3dae8fb4afcd9993a
meghagadad <meghagadad24@gmail.com>
2018-06-01 15:08:25 +0200

    r2score: -0.69

--- linear_split.py
+++ linear_split.py
@@ -10,6 +10,7 @@ import numpy as np
 from sklearn import linear_model
 import matplotlib.pyplot as plt
 from sklearn.metrics import mean_squared_error, r2_score
+np.set_printoptions(precision=2)
 
 #load dataset
 df = pd.read_csv('Flaveria.csv')#['N Level','Species','Plant Weight(g)']
@@ -24,12 +25,18 @@ data = df.copy()
 data_X = df.loc[:, df.columns != 'Plant Weight(g)']
 target_y = df.iloc[:,0]
 
+#split dataset into test and train
+#n_samples = data.shape[0]
+
+#data_X_train, target_y_train = data_X[:n_samples // 2], target_y[:n_samples // 2]
+#data_X_test, target_y_test = data_X[n_samples // 2:], target_y[n_samples // 2:]
+
 from sklearn.model_selection import train_test_split
 
 data_X_train, data_X_test, target_y_train, target_y_test = train_test_split(data_X, target_y, shuffle=True,
-                                                    test_size=0.5, random_state=0)
+                                                   test_size=0.5, random_state=0)
 
-# Create linear regression object
+# Create linear regression object#############
 regr = linear_model.LinearRegression()
 
 # Train the model using the training sets
@@ -41,7 +48,9 @@ target_y_pred = regr.predict(data_X_test)
 # The coefficients
 print('Coefficients: \n', regr.coef_)
 # The mean squared error
-print("Mean squared error: %.2f"
+print("Mean squared error: %f"
       % mean_squared_error(target_y_test, target_y_pred))
+
 # Explained variance score: 1 is perfect prediction
-print('Variance score or r2score: %.2f' % r2_score(target_y_test, target_y_pred))
+
+print('r2score: %.2f' % r2_score(target_y_test, target_y_pred))
