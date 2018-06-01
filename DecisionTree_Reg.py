3488d1bdf4e14aebcb03fbdd01f5fee1775f5cdb
meghagadad <meghagadad24@gmail.com>
2018-06-01 15:05:29 +0200

    R_2 score: -6.80038175464

--- DecisionTree_Reg.py
+++ DecisionTree_Reg.py
@@ -26,7 +26,13 @@ y = data['Plant Weight(g)']
 from sklearn.model_selection import train_test_split
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, shuffle=True,
-                                                   test_size=0.20, random_state=0)
+                                                   test_size=0.20, random_state=20)
+
+# Split data in train set and test set
+#n_samples = X.shape[0]
+
+#X_train, y_train = X[:n_samples // 2], y[:n_samples // 2]
+#X_test, y_test = X[n_samples // 2:], y[n_samples // 2:]
 
 # Training and Making Predictions
 from sklearn.tree import DecisionTreeRegressor  
@@ -39,16 +45,19 @@ regr.fit(X_train, y_train)
 # Make predictions using the testing set
 y_pred = regr.predict(X_test)
 
+#calculaate R2 score
+from sklearn.metrics import r2_score
+print('R_2 score:', r2_score(y_test, y_pred))
 
 #let's compare our predicted values with the actual values
 df=pd.DataFrame({'Actual':y_test, 'Predicted':y_pred})  
-print(df)
+#print(df)
 
 #evaluate performance of the regression algorithm
-from sklearn import metrics  
-print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))  
-print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))  
-print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred))) 
+#from sklearn import metrics  
+#print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))  
+#print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))  
+#print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred))) 
 
 
 
