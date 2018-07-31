78e3ce812233b9aa649d5008473edff089ec12ac
meghagadad <meghagadad24@gmail.com>
2018-07-31 12:30:43 +0200

    Add files via upload

--- DecisionTreeReg.py
+++ DecisionTreeReg.py
@@ -3,33 +3,38 @@
 Created on Thu May 31 16:35:58 2018
 
 @author: MeghaGadad
+Here I am using DecisionTreeRegressor Supervised method from Scikit Library
 """
 
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.metrics import r2_score
 
-#read two files for test and train
+#read two files for train and test
 train = pd.read_csv('Flaveria.csv')
+
+#file need for testing,completely new file with 6 datapoints removed from original
 test = pd.read_csv('Flaveria.csv')
 
-#get dummie values
+#get dummie values to deal with categorical values
 train = pd.get_dummies(train)
 test =  pd.get_dummies(test)
 
 #specify features and target
-features = ['N Level','Species','Plant Weight(g)']
-features = train.drop('Plant Weight(g)', axis=1)
+#features = ['N Level','Species','Plant Weight(g)']
+features = train.drop('Plant Weight(g)', axis=1) #it will store all column except weight
+features_test = test.drop('Plant Weight(g)', axis=1)
 
-#devide our dataaset into train and test
+#devide our dataaset into train and test 
 X_train = train[list(features)].values
 
 y_train = train['Plant Weight(g)'].values
 
-X_test =  test[list(features)].values
+X_test =  test[list(features_test)].values
 
 y_test =  test['Plant Weight(g)'].values
+
+
 
 ###########Training and Making Predictions using DecisionTreeRegressor #############
 from sklearn.tree import DecisionTreeRegressor  
@@ -49,8 +54,8 @@ df=pd.DataFrame({'Actual':y_test, 'Predicted':y_pred})
 #print(df)
 
 #evaluate performance of the regression algorithm
-#from sklearn import metrics  
-#print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))  
-#print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))  
-#print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred))) 
+from sklearn import metrics  
+print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))  
+print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))  
+print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred))) 
 
